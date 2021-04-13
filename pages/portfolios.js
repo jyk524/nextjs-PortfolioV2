86509cf0699173a58caa2cb59bfd60a096e507ef
James Kim <67247717+jyk524@users.noreply.github.com>
2021-04-13 17:04:04 -0400

    Data Fetch : testing data fetch from 'https://jsonplaceholder.typicode.com/posts'

--- pages/portfolios.js
+++ pages/portfolios.js
@@ -1,7 +1,8 @@
 import BaseLayout from "../components/layouts/BaseLayout";
 import axios from "axios";
 
-const Portfolios = () => {
+const Portfolios = ({ posts }) => {
+  console.log(posts);
   return (
     <BaseLayout>
       <h1>Portfolios h1</h1>
@@ -9,10 +10,16 @@ const Portfolios = () => {
   );
 };
 
-export default Portfolios;
+Portfolios.getInitialProps = async () => {
+  let posts = [];
+  try {
+    const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+    posts = res.data;
+  } catch (e) {
+    console.error(e);
+  }
 
-export async function getStaticProps(context) {
-  return {
-    props: {}, // will be passed to the page component as props
-  };
-}
+  return { posts: posts.slice(0, 10) };
+};
+
+export default Portfolios;
