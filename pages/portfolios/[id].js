9cc76a11df2f5a5921929129f4f156c1ced5c4f7
James Kim <67247717+jyk524@users.noreply.github.com>
2021-04-21 23:01:43 -0400

    Restructure : moved portfolio code to portfolio pages folder

---
+++ pages/portfolios/[id].js
@@ -0,0 +1,12 @@
+import BaseLayout from "../../components/layouts/BaseLayout";
+import axios from "axios";
+
+const Portfolio = () => {
+  return (
+    <BaseLayout>
+      <h1>Portfolio Page</h1>
+    </BaseLayout>
+  );
+};
+
+export default Portfolio;
