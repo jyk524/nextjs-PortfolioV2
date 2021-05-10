ae3c1f57aef017c19aa629019ef9afa59b1eaea1
James Kim <67247717+jyk524@users.noreply.github.com>
2021-05-09 22:42:48 -0400

    Add : Router to portfolio IDs to display portfolio page ID number

--- pages/portfolios/[id].js
+++ pages/portfolios/[id].js
@@ -1,10 +1,13 @@
 import BaseLayout from "../../components/layouts/BaseLayout";
+import { useRouter } from "next/router";
 import axios from "axios";
 
 const Portfolio = () => {
+  const router = useRouter();
   return (
     <BaseLayout>
       <h1>Portfolio Page</h1>
+      <h2>{router.query.id}</h2>
     </BaseLayout>
   );
 };
