accdecbdc7342ccc47eb3f3867dfbcf7bc8bfbe1
James Kim <67247717+jyk524@users.noreply.github.com>
2021-05-16 23:34:07 -0400

    Portfolio data requested and added to portfolio pages

--- pages/portfolios/[id].js
+++ pages/portfolios/[id].js
@@ -2,14 +2,31 @@ import BaseLayout from "../../components/layouts/BaseLayout";
 import { useRouter } from "next/router";
 import axios from "axios";
 
-const Portfolio = () => {
+const Portfolio = ({ post }) => {
   const router = useRouter();
   return (
     <BaseLayout>
       <h1>Portfolio Page</h1>
-      <h2>{router.query.id}</h2>
+      <h1>{post.title}</h1>
+      <p>BODY: {post.body}</p>
+      <p>ID: {post.id}</p>
     </BaseLayout>
   );
+};
+
+Portfolio.getInitialProps = async ({ query }) => {
+  let post = {};
+
+  try {
+    const res = await axios.get(
+      `https://jsonplaceholder.typicode.com/posts/${query.id}`
+    );
+    post = res.data;
+  } catch (e) {
+    console.error(e);
+  }
+
+  return { post };
 };
 
 export default Portfolio;
